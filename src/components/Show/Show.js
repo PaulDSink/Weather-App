c157fad0cfb41872728ee6f56ec3bb5933016b7d
elhameg <azizielhamg@johndeere.com>
2020-11-19 14:38:06 -0600

    added const

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import {Route, Link, Switch, Redirect} from 'react-router-dom'
-import axios from axios
+import axios from "axios"
 
-export default class Show extends Component {
-
-    render() {
+    const Show = () => {
         return (
-            <div>
-                <h1>this is a show page</h1>
+            <div className="show">
+                <div className="cards">
+                    <h1>United States</h1>
+                </div>
             </div>
-        )
-    }
-}
+        );
+    };
+export default Show;
