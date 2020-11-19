439bedf6a00a379abb27d690c038b15b53c4ec7f
elhameg <azizielhamg@johndeere.com>
2020-11-19 13:09:30 -0600

    created weather showpage

---
+++ src/components/PaulComponent/WeatherShowpage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class WeatherShowpage extends Component {
+
+    render() {
+        return (
+            <div>
+                this is a show page
+            </div>
+        )
+    }
+}
+
+export default WeatherShowpage
