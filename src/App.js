af42c05c22f1dfbfbef97cd3ffc2595ae4a66fa2
Paul Sink <SinkPaulD@JohnDeere.com>
2020-11-19 12:12:45 -0500

    initial set up of App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import {Route, Link, Switch, Redirect} from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
