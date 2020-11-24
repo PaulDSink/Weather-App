b01d773541e18c888668757b923fe46558891acd
jhovanec1 <hovanecjoseph@johndeere.com>
2020-11-24 08:10:51 -0600

    add time

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,12 @@ import './Header.css';
 export default class Header extends Component {
 
   render() {
+    let time = new Date().toLocaleTimeString();
+    
     return (
       <div className="Header">
-        <h1>Weather App</h1>
+        <h1>Weather App at {time}</h1>
+        
         {/* <nav>
           <ul>
             <li>HOME</li>
